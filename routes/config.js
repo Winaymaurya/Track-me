5c22d5779fb0297dd760877b1f6753aa7f3ed5c9
winaymaurya <vinaymaurya121103@gmail.com>
2026-03-12 18:40:55 +0530

    Ad

--- routes/config.js
+++ routes/config.js
@@ -5,8 +5,8 @@ const router = express.Router();
 // In a real production app, you might store this in MongoDB to change it without redeploying
 router.get('/version', (req, res) => {
     res.json({
-        latestVersion: '1.0.2', // Update this when you push a new AAB to Play Store
-        minRequiredVersion: '1.0.2', // Users below this will be forced to update
+        latestVersion: '1.0.3', //` Update this when you push a new AAB to Play Store
+        minRequiredVersion: '1.0.3', // Users below this will be forced to update
         updateUrl: 'https://play.google.com/store/apps/details?id=com.winaymauryatrackme.app',
         forceUpdate: true,
         message: 'A new version of TrackMe is available with better performance and new features! 🔥'
